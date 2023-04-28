06b590621d21c766242956426aa45de65709fc0b
Emre Cakmakcioglu <ecak0002@student.monash.edu>
2023-04-28 11:48:27 +1000

    Added token images and updated visualise.py.

--- source/visualise.py
+++ source/visualise.py
@@ -1,54 +1,63 @@
-import random
 import pygame
 
-
-
-BOARD_WIDTH = 800
-BOARD_HEIGHT = 700
-SLOT_SIZE = 100
-SLOT_SPACING = 10
-
-RED = (255,0,0)
-YELLOW = (255,255,0)
-
-pygame.display.set_caption("Connect-X Visulisation Demo")
-
-def draw_board(board):
-    pygame.draw.rect(screen, (0, 0, 255), (0, 0, BOARD_WIDTH, BOARD_HEIGHT))
-
-    for row in range(len(board)):
-        for col in range(len(board[0])):
-            if board[row][col] == 1:
-                color = RED
-            elif board[row][col] == 2:
-                color = YELLOW
-            else:
-                color = (255, 255, 255)
-            pygame.draw.circle(screen, color, (col * (SLOT_SIZE + SLOT_SPACING) + SLOT_SIZE // 2,
-                                               row * (SLOT_SIZE + SLOT_SPACING) + SLOT_SIZE // 2), SLOT_SIZE // 2)
-
-pygame.init()
-screen = pygame.display.set_mode((BOARD_WIDTH, BOARD_HEIGHT))
-
-board = [
+test_board = [
     [None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None],
-    [None, None, None, 2, None, None, None],
-    [1, None, 1, 1, 2, None, 2]
+    [None, 1, None, 2, None, None, None],
+    [1, 2, 1, 1, 2, None, 2]
 ]
 
+def visualise(board):
+    """
+    :param board: a 2d array with None, 1 or 2 in each subarray
+    :return: a pygame instance displaying the board. It does not indicate a winning move.
+    """
 
-print(board)
+    SLOT_SIZE = 105
+    BOARD_WIDTH = len(board[0]) * SLOT_SIZE
+    BOARD_HEIGHT = len(board) * SLOT_SIZE
 
-while True:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
 
-        draw_board(board)
-        pygame.display.flip()
+    red_chip = pygame.image.load("assets/images/redchip.png")
+    yellow_chip = pygame.image.load("assets/images/yellowchip.png")
+    empty = pygame.image.load("assets/images/empty.png")
 
+    updated_size = (90, 90)
+
+    red_scaled = pygame.transform.scale(red_chip, updated_size)
+    yellow_scaled = pygame.transform.scale(yellow_chip, updated_size)
+    empty_scaled = pygame.transform.scale(empty, updated_size)
+
+    pygame.display.set_caption("Connect-X Visualisation Demo")
+
+    def draw_board(board):
+        pygame.draw.rect(screen, (42, 42, 199), (0, 0, BOARD_WIDTH, BOARD_HEIGHT))
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == 1:
+                    screen.blit(red_scaled, (col * SLOT_SIZE, row * SLOT_SIZE))
+
+                elif board[row][col] == 2:
+                    screen.blit(yellow_scaled, (col * SLOT_SIZE, row * SLOT_SIZE))
+                else:
+                    color = (255, 255, 255)
+                    screen.blit(empty_scaled, (col * SLOT_SIZE, row * SLOT_SIZE))
+
+    pygame.init()
+    screen = pygame.display.set_mode((BOARD_WIDTH, BOARD_HEIGHT))
+
+    while True:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            draw_board(board)
+            pygame.display.flip()
+
+def test():
+    return visualise(test_board)
