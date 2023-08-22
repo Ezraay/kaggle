bab05e4cf07040e46d5d2c151dd647a63c19117d
JackyZhaoMonash <129266503+JackyZhaoMonash@users.noreply.github.com>
2023-08-22 15:38:19 +1000

    Time move - temp save

--- source/agents/random_agent.py
+++ source/agents/random_agent.py
@@ -2,7 +2,6 @@ import random
 
 from core.agent import Agent
 from core.board import Board
-import time as time
 
 class RandomAgent(Agent):
     def get_move(self, board: Board, my_piece: int) -> int:
@@ -11,8 +10,3 @@ class RandomAgent(Agent):
         options = [x for x in list(range(board.width)) if board.can_make_move(x)]
         return random.choice(options)
     
-    def time_move(self, board: Board, my_piece: int):
-        start = time.time()
-        self.get_move(board,my_piece)
-        res = time.time() - start
-        print("This takes {res} seconds to respond.") 
