7c69ae1cb98106d0e0d2a62b93591b9fa94e22a3
JackyZhaoMonash <129266503+JackyZhaoMonash@users.noreply.github.com>
2023-08-22 13:55:30 +1000

    This give any agent abilty to time its get_move() that inherits the RandomAgent

--- source/agents/random_agent.py
+++ source/agents/random_agent.py
@@ -2,7 +2,7 @@ import random
 
 from core.agent import Agent
 from core.board import Board
-
+import time as time
 
 class RandomAgent(Agent):
     def get_move(self, board: Board, my_piece: int) -> int:
@@ -10,3 +10,9 @@ class RandomAgent(Agent):
         # generate a list possible from the class Board and return a random move
         options = [x for x in list(range(board.width)) if board.can_make_move(x)]
         return random.choice(options)
+    
+    def time_move(self, board: Board, my_piece: int):
+        start = time.time()
+        self.get_move(board,my_piece)
+        res = time.time() - start
+        print("This takes {res} seconds to respond.") 
